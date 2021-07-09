4069b718f54056b50a413ef17a6de7978ee58251
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-07-09 15:17:55 -0300

    C3 - Adicionado requisito 3

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+//Requisito 1
 const createHeader = document.createElement('header');
 document.body.appendChild(createHeader);
 const createH1 = document.createElement('h1');
 createHeader.appendChild(createH1).innerText = 'Minha Lista de Tarefas';
-
+//Criando o conteudo
+const createMain = document.createElement('main');
+document.body.appendChild(createMain);
+//Requisito 2
 const createParagraph = document.createElement('p');
-document.body.appendChild(createParagraph).innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+createMain.appendChild(createParagraph).innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 createParagraph.id = 'funcionamento';
-
+//Requisito 3
+const createInput = document.createElement('input');
+createMain.appendChild(createInput);
+createInput.id = 'texto-tarefa'
