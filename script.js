3154235894158224d4c4936949b71a17e715e8df
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-07-09 15:02:36 -0300

    C1 - Adicionado requisito 1

---
+++ script.js
@@ -0,0 +1,6 @@
+const createHeader = document.createElement('header');
+document.body.appendChild(createHeader);
+const createH1 = document.createElement('h1');
+createHeader.appendChild(createH1).innerText = 'Minha Lista de Tarefas';
+
+
