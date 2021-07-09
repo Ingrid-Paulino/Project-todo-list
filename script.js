6607d391c0cabdbf8eaf1e4f849bed4de6290ffa
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-07-09 15:09:46 -0300

    C2 - Adicionado requisito 2

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ document.body.appendChild(createHeader);
 const createH1 = document.createElement('h1');
 createHeader.appendChild(createH1).innerText = 'Minha Lista de Tarefas';
 
+const createParagraph = document.createElement('p');
+document.body.appendChild(createParagraph).innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+createParagraph.id = 'funcionamento';
 
