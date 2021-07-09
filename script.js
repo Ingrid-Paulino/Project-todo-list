14a685a4bb003117aa29912eafdad4e2cdb396c0
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2021-07-09 16:48:15 -0300

    C6 - Adicionado Requisito 7

--- script.js
+++ script.js
@@ -3,22 +3,27 @@ const createHeader = document.createElement('header');
 document.body.appendChild(createHeader);
 const createH1 = document.createElement('h1');
 createHeader.appendChild(createH1).innerText = 'Minha Lista de Tarefas';
+
 //Criando o conteudo
 const createMain = document.createElement('main');
 document.body.appendChild(createMain);
+
 //Requisito 2
 const createParagraph = document.createElement('p');
 createMain.appendChild(createParagraph).innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 createParagraph.id = 'funcionamento';
+
 //Requisito 3
 const createInput = document.createElement('input');
 createMain.appendChild(createInput);
 createInput.id = 'texto-tarefa';
+
 //Requisito 4
 const createListOrder = document.createElement('ol');
 createMain.appendChild(createListOrder);
 createListOrder.id = 'lista-tarefas';
-//Requisito 5
+
+//Requisito 5 e 6
 const createBtn = document.createElement('button') 
 createMain.appendChild(createBtn).innerText = 'Criar Tarefa';
 createBtn.id = 'criar-tarefa'
@@ -30,8 +35,20 @@ const listText = document.getElementById('lista-tarefas')
 function addText() {
     const text = inputText.value;
     const listItem = document.createElement('li');
+    listItem.className = 'color'
     listText.appendChild(listItem);
     listItem.innerText = text;
     inputText.value = '';
 }
 btnAddText.addEventListener('click', addText)
+
+//Requisito 7
+const listColor = document.getElementById('lista-tarefas');
+const btnListColor = document.getElementsByClassName('color'); 
+
+function colorList(event) {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+}
+listColor.addEventListener('click', colorList);
+
+
